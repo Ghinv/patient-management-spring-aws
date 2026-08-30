ee6122669ae2a7bec3293bd9cba4e10177a64efa
sufyannordin <sufyan9747@gmail.com>
2026-08-31 01:21:14 +0800

    - Fix dependency so that the kafka works

--- patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
+++ patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -1,5 +1,4 @@
 package com.pm.patientservice.kafka;
-
 import com.pm.patientservice.model.Patient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +9,8 @@ import patient.events.PatientEvent;
 @Service
 public class KafkaProducer {
 
-    private static final Logger log = LoggerFactory.getLogger(KafkaProducer.class);
+    private static final Logger log = LoggerFactory.getLogger(
+            KafkaProducer.class);
     private final KafkaTemplate<String, byte[]> kafkaTemplate;
 
     public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
@@ -18,11 +18,13 @@ public class KafkaProducer {
     }
 
     public void sendEvent(Patient patient) {
-        PatientEvent event = PatientEvent.newBuilder().setPatientId(patient.getId().toString())
+        PatientEvent event = PatientEvent.newBuilder()
+                .setPatientId(patient.getId().toString())
                 .setName(patient.getName())
                 .setEmail(patient.getEmail())
                 .setEventType("PATIENT_CREATED")
                 .build();
+
         try {
             kafkaTemplate.send("patient", event.toByteArray());
         } catch (Exception e) {
